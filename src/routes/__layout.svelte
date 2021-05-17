67c627a6e1cc46bc90a1449402148ea90364eceb
vikingviolinist <misopetrik@gmail.com>
2021-05-17 19:51:32 +0200

    clown_face: Finish initial mockup

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Header from "$lib/Header/index.svelte";
+  import "../app.css";
+</script>
+
+<Header />
+
+<main>
+  <slot />
+</main>
+
+<footer>
+  <p>
+    Check out the code on <a
+      href="https://github.com/keyprez/keyprez"
+      target="blank">GitHub</a
+    >
+  </p>
+</footer>
+
+<style>
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+
+  footer {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px;
+    background-color: var(--primary-color);
+  }
+
+  footer a {
+    font-weight: bold;
+  }
+
+  @media (min-width: 480px) {
+    footer {
+      padding: 40px 0;
+    }
+  }
+</style>
