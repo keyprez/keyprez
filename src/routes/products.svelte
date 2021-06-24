1545f6a37e5ea16d883e2eb202d7fa83176322d5
csandven <christian.sandven@kindly.ai>
2021-06-24 22:05:10 +0200

    WIP category navigation

--- src/routes/products.svelte
+++ src/routes/products.svelte
@@ -1,16 +1,28 @@
-<script context="module">
+<script>
   import { inventory } from '../stores/inventory';
   import Product from '$lib/Product.svelte';
   export const prerender = true;
+
+  const urlParams = new URLSearchParams(typeof window !== 'undefined' && window.location.search);
+  const activeCategory = urlParams.get('category');
+
+  const categories = Array.from(new Set($inventory.map(value => value.category)));
+  const activeProducts = activeCategory ? $inventory.filter(value => value.category === activeCategory) : $inventory;
 </script>
 
 <svelte:head>
   <title>Products</title>
 </svelte:head>
 
+<nav>
+  {#each categories as category}
+    <a sveltekit:prefetch href="?category={category}">{category}</a>
+  {/each}
+</nav>
+
 <h1>Products</h1>
 <div class="products">
-  {#each $inventory as product}
+  {#each activeProducts as product}
     <Product {product} />
   {/each}
 </div>
