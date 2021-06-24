acae7e030b30425b342b91691a0227eb7eee43af
csandven <christian.sandven@kindly.ai>
2021-06-24 20:59:40 +0200

    :sparkles: Add localStorage support for cart store

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,3 +1,25 @@
 import { writable } from 'svelte/store';
 
-export const cartProducts = writable([]);
+const hasLocalStorage = () => typeof window !== 'undefined' && window.localStorage
+
+const createWriteableStore = (key, startValue) => {
+    const { subscribe, set } = writable(startValue);
+  
+    return {
+        subscribe,
+        set,
+        useLocalStorage: () => {
+            const json = hasLocalStorage() && localStorage.getItem(key);
+            if (json) {
+                set(JSON.parse(json));
+            }
+            
+            subscribe(current => {
+                if (hasLocalStorage())
+                    localStorage.setItem(key, JSON.stringify(current));
+            });
+        }
+    };
+}
+
+export const cartProducts = createWriteableStore('cart', hasLocalStorage() ? localStorage.getItem('cart') || [] : []);
