c1f897159a7f662f540ada5fd402effd8fc03b2b
vikingviolinist <misopetrik@gmail.com>
2021-09-04 20:22:23 +0200

    :sparkles: 404 page
    Replace default 404 page with a custom one

---
+++ src/routes/__error.svelte
@@ -0,0 +1,12 @@
+<script context="module">
+  export const prerender = true;
+</script>
+
+<svelte:head>
+  <title>Not found</title>
+</svelte:head>
+
+<div>
+  <img src="404.png" alt="404 keycap" />
+  <h2><a href="/">Home</a></h2>
+</div>
