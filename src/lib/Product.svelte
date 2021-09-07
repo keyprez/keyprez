7c6b599043929cda7a8863c5e7962f5b8662b233
Michal Petrik <misopetrik@gmail.com>
2021-09-07 08:26:19 +0200

    ✨ Stripe checkout (#29)
    * :sparkles: Add stripe checkout
    
    * :wrench: Apply code review suggestions

--- src/lib/Product.svelte
+++ src/lib/Product.svelte
@@ -3,9 +3,33 @@
   import { fade } from 'svelte/transition';
 
   import { products } from '../stores/products';
+  import config from '../../config';
+
+  const { endpoint, stripePublishableKey } = config;
 
   const productName = $page.path.replace('/', '');
-  const { name, description, price } = $products.find((product) => product.name === productName);
+  const { name, description, price, priceId } = $products.find((product) => product.name === productName);
+  let submitting = false;
+
+  /* global Stripe */
+  const stripe = Stripe(stripePublishableKey);
+
+  const handleRedirect = async () => {
+    submitting = true;
+    try {
+      const res = await fetch(`${endpoint}/checkout`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, priceId }),
+      });
+      const data = await res.json();
+      if (data.error) throw new Error(data.error);
+      await stripe.redirectToCheckout({ sessionId: data.id });
+    } catch (err) {
+      console.error(`Error creating Stripe checkout session: ${err}`);
+    }
+    submitting = false;
+  };
 </script>
 
 <div class="container" in:fade>
@@ -37,7 +61,7 @@
       Proin eleifend bibendum nunc, a ornare mi lacinia nec. Pellentesque suscipit sapien at sodales vestibulum. Etiam
       finibus leo non nisi hendrerit, non eleifend leo semper. Aenean et fringilla massa.
     </p>
-    <button>BUY</button>
+    <button on:click|preventDefault={handleRedirect} disabled={submitting}>{submitting ? 'PROCESSING' : 'BUY'}</button>
     <h2>&#36;<span>{price}</span></h2>
   </div>
 </div>
