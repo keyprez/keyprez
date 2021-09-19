88163784489c8b724725dbc7b26f386d530d6abf
Michal Petrik <misopetrik@gmail.com>
2021-09-19 08:57:53 +0200

    :sparkles: Dynamic routing and cache (#32)

---
+++ src/utils/redirectToCheckout.ts
@@ -0,0 +1,18 @@
+import { endpoint, stripePublishableKey } from '../../config';
+
+export const redirectToCheckout = async (productName: string, priceId: string): Promise<void> => {
+  /* global Stripe */
+  const stripe = Stripe(stripePublishableKey);
+  try {
+    const res = await fetch(`${endpoint}/checkout`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ productName, priceId }),
+    });
+    const data = await res.json();
+    if (data.error) throw new Error(data.error);
+    await stripe.redirectToCheckout({ sessionId: data.id });
+  } catch (err) {
+    console.error(`Error creating Stripe checkout session: ${err}`);
+  }
+};
