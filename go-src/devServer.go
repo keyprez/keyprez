98b796a148b84e48ba8658d72af15c3d6bb13731
csandven <chs_91@hotmail.com>
2021-10-31 13:39:09 +0100

    🔨 Forward more request parameters

--- go-src/devServer.go
+++ go-src/devServer.go
@@ -1,17 +1,50 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 
 	"github.com/aws/aws-lambda-go/events"
 
 	"github.com/keyprez/keyprez/go-src/lib/products"
 )
 
+type ProductRequestPost struct {
+	A string `json:"a"`
+}
+
+func transformRequest(req *http.Request) events.APIGatewayProxyRequest {
+	pathParams := mux.Vars(req)
+	defer req.Body.Close()
+
+	var mapInterface ProductRequestPost
+	_ = json.NewDecoder(req.Body).Decode(&mapInterface)
+
+	queryStringParams := make(map[string]string)
+	multiValueQueryStringParams := make(map[string][]string)
+	for key, val := range req.URL.Query() {
+		if len(val) > 1 {
+			multiValueQueryStringParams[key] = val
+		} else {
+			queryStringParams[key] = val[0]
+		}
+	}
+
+	return events.APIGatewayProxyRequest{
+		HTTPMethod:                      req.Method,
+		Headers:                         headers(req),
+		PathParameters:                  pathParams,
+		QueryStringParameters:           queryStringParams,
+		MultiValueQueryStringParameters: multiValueQueryStringParams,
+	}
+}
+
 func productsHandle(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Received request")
-	response, err := products.ProductsHandler(events.APIGatewayProxyRequest{})
+	response, err := products.ProductsHandler(transformRequest(req))
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -21,20 +54,24 @@ func productsHandle(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(response.Body))
 }
 
-func headers(w http.ResponseWriter, req *http.Request) {
-
+func headers(req *http.Request) map[string]string {
+	m := make(map[string]string)
 	for name, headers := range req.Header {
 		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
+			m[name] = h
 		}
 	}
+	return m
 }
 
 func main() {
-	http.HandleFunc("/products", productsHandle)
-	err := http.ListenAndServe(":8090", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/products", productsHandle)
+
+	fmt.Println("Listening on port 8090")
+	err := http.ListenAndServe(":8090", r)
 
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
 }
