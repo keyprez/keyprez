43c0939c0b43c6956d5cff8e1fbbbc3e409848e5
Michal Petrik <misopetrik@gmail.com>
2022-01-30 18:22:35 +0100

    ✨ Pass customer stripe ID to checkout session

---
+++ src/utils/getCustomerStripeId.ts
@@ -0,0 +1,15 @@
+import { endpoint } from '../config';
+
+export default async (email: string): Promise<void> => {
+  try {
+    const res = await fetch(`${endpoint}/customers`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email }),
+    });
+    const { StripeID: customerStripeId } = await res.json();
+    return customerStripeId;
+  } catch (err) {
+    console.error(`Error getting customer Stripe ID: ${err}`);
+  }
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as capitalize } from './capitalize';
 export { default as fetchProducts } from './fetchProducts';
+export { default as getCustomerStripeId } from './getCustomerStripeId';
 export { default as getProductBySlug } from './getProductBySlug';
 export { default as redirectToCheckout } from './redirectToCheckout';
