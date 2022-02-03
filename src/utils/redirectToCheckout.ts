d8735a7ea05d20614df802032b070de439336988
Michal Petrik <misopetrik@gmail.com>
2022-02-03 20:36:14 +0100

    :goal_net: Enhance checkout error handling

--- src/utils/redirectToCheckout.ts
+++ src/utils/redirectToCheckout.ts
@@ -1,17 +1,10 @@
-import { endpoint, stripePublishableKey } from '../config';
+import { stripePublishableKey } from '../config';
 
-export default async (productName: string, priceId: string, customerStripeId: string): Promise<void> => {
-  /* global Stripe */
-  const stripe = Stripe(stripePublishableKey);
+export default async (sessionId: string): Promise<void> => {
   try {
-    const res = await fetch(`${endpoint}/orders/checkout`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ productName, priceId, customerStripeId }),
-    });
-    const { id: sessionId } = await res.json();
-    await stripe.redirectToCheckout({ sessionId });
+    const stripe = Stripe(stripePublishableKey);
+    return await stripe.redirectToCheckout({ sessionId });
   } catch (err) {
-    console.error(`Error creating Stripe checkout session: ${err}`);
+    console.error(`Error redirecting to Stripe checkout: ${err}`);
   }
 };
