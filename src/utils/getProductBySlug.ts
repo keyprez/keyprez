c77db64367f3dd8a271f6670379af8452aedf652
Michal Petrik <misopetrik@gmail.com>
2022-02-20 10:37:04 +0100

    :goal_net: Catch errors in getProductBySlug

--- src/utils/getProductBySlug.ts
+++ src/utils/getProductBySlug.ts
@@ -2,7 +2,11 @@ import { fetchProducts } from './index';
 import type { Product } from './interfaces';
 
 export default async (slug: string): Promise<Product | void> => {
-  const products: Product[] = await fetchProducts();
-  const product: Product | undefined = products.find((product) => product.Slug.toLowerCase() === slug.toLowerCase());
-  return product;
+  try {
+    const products: Product[] = await fetchProducts();
+    const product: Product | undefined = products.find((product) => product.Slug.toLowerCase() === slug.toLowerCase());
+    return product;
+  } catch (err) {
+    console.error(`Error getting product by slug: ${err}`);
+  }
 };
