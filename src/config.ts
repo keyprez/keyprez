c4c2fd36679f3f849c5b30e0fb8f3b8d65f1c2fc
Christian Sandven <chs_91@hotmail.com>
2022-06-29 19:49:12 +0200

    Test build

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,4 @@ export const endpoint = process.env.NODE_ENV === 'production' ? '/.netlify/funct
 // The key needs to be replaced with a production one once we go live.
 export const stripePublishableKey =
   'pk_test_51IzFveLwcEKoBoonzTPmtwFhbwqix42XQtnxeHPtDb2IutljsV75FQI9jVi9JOCxBHYxQLvy3eVwpTWXYpmVDplN00ZeSz3HSi';
+export const test = 3;
