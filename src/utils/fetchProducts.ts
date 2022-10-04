a5f070392e6468701bb166df44793644404470ab
vikingviolinist <misopetrik@gmail.com>
2022-10-04 15:05:41 +0200

    ♻️  Only run cache fetching in browser

--- src/utils/fetchProducts.ts
+++ src/utils/fetchProducts.ts
@@ -1,20 +1,24 @@
+import { browser } from '$app/environment';
 import { endpoint } from '../config';
 import type { Product, Cache } from './interfaces';
 
 export default async (): Promise<Product[]> => {
-  const lifespan = 3600; // 1 hour
-  const cacheStr: string | null = localStorage.getItem('products');
-  let cache: Cache | null = null;
   let products: Product[] = [];
-  let expired = false;
 
-  if (cacheStr) {
-    cache = JSON.parse(cacheStr) as Cache;
-    expired = Math.floor(Date.now() / 1000) - cache.cacheTime > lifespan;
-  }
+  if (browser) {
+    const lifespan = 3600; // 1 hour
+    const cacheStr: string | null = localStorage.getItem('products');
+    let cache: Cache | null = null;
+    let expired = false;
 
-  if (cache && !expired) {
-    return cache.products;
+    if (cacheStr) {
+      cache = JSON.parse(cacheStr) as Cache;
+      expired = Math.floor(Date.now() / 1000) - cache.cacheTime > lifespan;
+    }
+
+    if (cache && !expired) {
+      return cache.products;
+    }
   }
 
   // Fetch products from the server and save in localStorage
