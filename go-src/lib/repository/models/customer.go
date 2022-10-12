369872ba375b8a14c7f231d1f9ab11f6810fcf3f
vikingviolinist <misopetrik@gmail.com>
2022-10-12 11:23:36 +0200

    ✨ Update customer shipping details if necessary

--- go-src/lib/repository/models/customer.go
+++ go-src/lib/repository/models/customer.go
@@ -3,23 +3,30 @@ package models
 import (
 	"time"
 
+	"github.com/go-playground/validator"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Customer struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty"`
 	StripeID   string             `bson:"stripeid,omitempty"`
-	Email      string             `bson:"email,omitempty"`
-	Name       string             `bson:"name,omitempty"`
-	City       string             `bson:"city,omitempty"`
-	Country    string             `bson:"country,omitempty"`
-	Line1      string             `bson:"line1,omitempty"`
-	Line2      string             `bson:"line2,omitempty"`
-	PostalCode string             `bson:"postalCode,omitempty"`
+	Email      string             `bson:"email,omitempty" validate:"required,email"`
+	Name       string             `bson:"name,omitempty" validate:"required,min=2"`
+	City       string             `bson:"city,omitempty" validate:"required"`
+	Country    string             `bson:"country,omitempty" validate:"required,eq=NO|eq=SE|eq=DK"`
+	Line1      string             `bson:"line1,omitempty" validate:"required"`
+	Line2      string             `bson:"line2,omitempty" validate:"omitempty,min=2"`
+	PostalCode string             `bson:"postalCode,omitempty" validate:"required"`
 	State      string             `bson:"state,omitempty"`
 	Created    time.Time          `bson:"created,omitempty"`
 }
 
-func (ns *Customer) IsValid() bool {
+func (ns *Customer) HasValidID() bool {
 	return !ns.ID.IsZero()
+}
+
+func (customer *Customer) IsValid() bool {
+	validate := validator.New()
+	err := validate.Struct(customer)
+	return err == nil
 }
