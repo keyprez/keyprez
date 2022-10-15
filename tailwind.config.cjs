af0b9c5c1da7ef275b3a8a73b2aff8e408fc0ae8
vikingviolinist <misopetrik@gmail.com>
2022-10-15 09:29:59 +0200

    ♻️  Refactor Loader

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulse: {
+          '0%, 100%': {
+            transform: 'scale(1.5)',
+            opacity: 0.3,
+          },
+          '50%': {
+            transform: 'scale(2)',
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        pulse: 'pulse 3s infinite ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
