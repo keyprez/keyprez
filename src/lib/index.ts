beb9a638eca04315f0800e77671ea4b9f80ebd24
vikingviolinist <misopetrik@gmail.com>
2022-10-16 12:27:04 +0200

    💄 Apply color toggling

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,8 @@ export { default as Button } from './Button.svelte';
 export { default as Checkbox } from './Checkbox.svelte';
 export { default as Footer } from './Footer.svelte';
 export { default as Header } from './Header.svelte';
+export { default as KeyprezLogo } from './KeyprezLogo.svelte';
+export { default as KeyprezName } from './KeyprezName.svelte';
 export { default as Loader } from './Loader.svelte';
 export { default as Product } from './Product.svelte';
+export { default as ThemeToggle } from './ThemeToggle.svelte';
