4a73bccfebc3d874ddccaae53fb705601ed2c5f7
vikingviolinist <misopetrik@gmail.com>
2022-10-17 15:36:48 +0200

    🏷  Type cart store

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,5 @@
 import { writable } from 'svelte/store';
+import type { Writable } from 'svelte/store';
+import type { CartItem } from './utils/interfaces';
 
-export const cart = writable([]);
+export const cart: Writable<CartItem[]> = writable([]);
