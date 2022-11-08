5081c03e3a01030f002475eac6787ae1c9a9407e
vikingviolinist <misopetrik@gmail.com>
2022-11-08 19:38:51 +0100

    💄 Fix some styles

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -14,23 +14,24 @@
 
 <SvelteSeo title="Keyprez - Success" />
 
-<div class="flex flex-col gap-4">
-  <h1>Thank you for purchasing <span class="text-black">{description}</span> kit</h1>
+<div class="flex flex-col gap-4 items-center">
+  <h1>Thank you for your purchase 😊</h1>
   <img src={`/${description.toLocaleLowerCase()}.jpg`} alt={description} />
-  <h2>Summary</h2>
+  <h2 class="text-lg font-bold">Summary</h2>
   <div class="text-left">
-    <h3>Order ID: <strong class="text-black">{paymentId}</strong></h3>
-    <h3>Name: <strong class="text-black">{name}</strong></h3>
-    <h3>City: <strong class="text-black">{city}</strong></h3>
-    <h3>Country: <strong class="text-black">{country}</strong></h3>
-    <h3>Line1: <strong class="text-black">{line1}</strong></h3>
+    <h3>Order ID: <strong class="text-black dark:text-teal-800">{paymentId}</strong></h3>
+    <h3>Name: <strong class="text-black dark:text-teal-800">{name}</strong></h3>
+    <h3>City: <strong class="text-black dark:text-teal-800">{city}</strong></h3>
+    <h3>Country: <strong class="text-black dark:text-teal-800">{country}</strong></h3>
+    <h3>Line1: <strong class="text-black dark:text-teal-800">{line1}</strong></h3>
     {#if line2}
-      <h3>Line2: <strong class="text-black">{name}</strong></h3>
+      <h3>Line2: <strong class="text-black dark:text-teal-800">{name}</strong></h3>
     {/if}
-    <h3>Postal Code: <strong class="text-black">{postalCode}</strong></h3>
+    <h3>Postal Code: <strong class="text-black dark:text-teal-800">{postalCode}</strong></h3>
     {#if state}
-      <h3>State: <strong class="text-black">{state}</strong></h3>
+      <h3>State: <strong class="text-black dark:text-teal-800">{state}</strong></h3>
     {/if}
-    <h3>Confirmation email has been sent to <strong class="text-black">{email}</strong></h3>
+    <br />
+    <h3>Confirmation email has been sent to <strong class="text-black dark:text-teal-800">{email}</strong></h3>
   </div>
 </div>
