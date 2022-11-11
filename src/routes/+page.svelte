b49530258db9f640c6b8c25e82e9ac87a8aca486
vikingviolinist <misopetrik@gmail.com>
2022-11-11 13:38:00 +0100

    ⚡ Call api only on refresh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,45 @@
-<script>
+<script lang="ts">
+  import { onDestroy } from 'svelte';
+  import { visited } from '../store';
   import SvelteSeo from 'svelte-seo';
-  import { fade } from 'svelte/transition';
-  import { Loader, Product } from '$lib';
+  import { Product, ProductSkeleton } from '$lib';
 
-  import { fetchProducts } from '/src/utils';
+  import { fetchProducts, cache } from '../store';
 
-  const response = fetchProducts();
+  const cachedProducts = cache.get('products');
+
+  const fetchedProducts = fetchProducts();
+
+  let shouldAnimate: boolean;
+
+  const unsubscribe = visited.subscribe((obj) => {
+    shouldAnimate = !obj.home;
+  });
+
+  onDestroy(() => {
+    visited.update((obj) => ({ ...obj, home: true }));
+    unsubscribe();
+  });
 </script>
 
 <SvelteSeo title="keyprez" description="New keyboard shop in progress..." />
 
-<div class="flex content-center justify-between flex-wrap gap-6" in:fade={{ duration: 1000 }}>
-  {#await response}
-    <Loader />
-  {:then products}
-    {#each products as product}
-      <Product {product} />
+<div class="flex content-center justify-center flex-wrap gap-6">
+  {#if cachedProducts}
+    {#each cachedProducts as product, index (product.priceId)}
+      <Product {product} index={index + 1} {shouldAnimate} />
     {/each}
-  {:catch}
-    <h1>Something went wrong :(</h1>
-  {/await}
+  {:else}
+    {#await $fetchedProducts}
+      {#each [1, 2, 3, 4] as _}
+        <ProductSkeleton />
+      {/each}
+    {:then products}
+      {#each products as product, index (product.priceId)}
+        <Product {product} index={index + 1} {shouldAnimate} />
+      {/each}
+    {:catch}
+      <h1>Something went wrong :(</h1>
+    {/await}
+  {/if}
 </div>
--- src/routes/product/[product]/+page.svelte
+++ src/routes/product/[product]/+page.svelte
@@ -1,65 +1,36 @@
-<script>
-  import SvelteSeo from 'svelte-seo';
+<script lang="ts">
   import { page } from '$app/stores';
-  import { cart } from '../../../store';
 
-  import { Button } from '$lib';
-  import { getProductBySlug } from '/src/utils';
-  import { capitalize } from 'lodash';
+  import { ProductSkeleton } from '$lib';
+  import ProductDetail from '../ProductDetail.svelte';
+  import { cache, fetchProducts } from '../../../store';
 
-  const productSlug = $page.url.pathname.replace('/product/', '');
+  import type { Product } from '../../../utils/interfaces';
 
-  const addToCart = (product) => {
-    const cartItem = $cart.find((item) => item.priceId === product.priceId);
-    if (cartItem) {
-      cartItem.quantity += 1;
-      $cart = $cart;
-      return;
-    }
-    $cart = [...$cart, { ...product, quantity: 1 }];
+  const cachedProducts: Product[] = cache.get('products');
+  const fetchedProducts = fetchProducts();
+
+  const slug = $page.url.pathname.replace('/product/', '');
+
+  const getProduct = (prods: Product[]) => {
+    const product = prods.find((p: Product) => p.slug.toLowerCase() === slug.toLowerCase());
+    if (!product) throw new Error('No product found');
+    return product;
   };
+
+  const cachedProduct = getProduct(cachedProducts);
 </script>
 
 <div class="flex flex-col xl:flex-row gap-4 md:gap-8 w-full">
-  {#await getProductBySlug(productSlug)}
-    <h1>LOADING...</h1>
-  {:then product}
-    <SvelteSeo title={`Keyprez - ${capitalize(product.name)}`} />
-    <img
-      class="shadow-lg flex-[60%] object-cover w-full rounded-lg"
-      src={`/${product.name.toLowerCase()}.jpg`}
-      alt={product.name}
-    />
-    <div class="flex flex-col gap-4 items-center flex-[40%]">
-      <h1 class="flex text-black text-2xl">{product.description.toUpperCase()}</h1>
-      <p>
-        Curabitur quis facilisis sapien. Cras luctus elit in ante tincidunt aliquet. Praesent interdum euismod felis
-        eget condimentum. Nam cursus pulvinar lacus at ultricies. Aliquam tempor consequat est, eu iaculis ipsum
-        imperdiet non. Duis placerat aliquam justo, id sodales risus tempus eget. Quisque ornare, nunc a laoreet
-        sagittis, diam sapien varius odio, nec posuere ligula neque eu nunc. Nulla hendrerit luctus turpis, sed egestas
-        enim iaculis sit amet. Nam nec nunc quis nibh rutrum condimentum. Vestibulum risus risus, tempor in nisl sit
-        amet, luctus gravida mauris. Donec vestibulum est quis blandit tincidunt. Curabitur lorem ex, porta id pretium
-        eu, scelerisque sit amet lacus. Sed est ante, aliquet ut semper in, sagittis pretium leo. Nam gravida pharetra
-        ex quis gravida. Suspendisse arcu lacus, sodales in est sit amet, faucibus euismod enim.
-      </p>
-      <p>
-        Integer nec odio nec mi ornare auctor in ut libero. Pellentesque vel finibus sem. Donec vel lorem lorem. Fusce
-        eu purus lobortis risus sagittis rutrum. Mauris gravida efficitur posuere. Praesent a libero ex. Nunc non
-        sodales mauris. Ut commodo lectus sed est vestibulum, eu pulvinar mauris venenatis. Aenean tempor sapien blandit
-        ex egestas rhoncus. Suspendisse sagittis dui nisl, non elementum neque eleifend in. Fusce cursus neque sed arcu
-        fringilla pharetra. Fusce lacinia, augue id ultrices dictum, nibh nibh placerat mi, et tristique eros justo ut
-        nisi. In tempus pretium hendrerit. Duis ac malesuada nulla. Nullam pellentesque purus id eros sagittis, egestas
-        mollis sapien dignissim.
-      </p>
-      <p>
-        Suspendisse quis odio vel augue gravida faucibus id at eros. Donec sit amet ornare felis. Praesent dapibus nisl
-        leo, sit amet auctor lorem tincidunt eget. Aenean vestibulum pretium sapien, et pulvinar libero consequat ac.
-        Proin eleifend bibendum nunc, a ornare mi lacinia nec. Pellentesque suscipit sapien at sodales vestibulum. Etiam
-        finibus leo non nisi hendrerit, non eleifend leo semper. Aenean et fringilla massa.
-      </p>
-
-      <Button text="Add to cart" onClick={() => addToCart(product)} />
-      <h2><span>{product.price} NOK</span></h2>
-    </div>
-  {/await}
+  {#if cachedProduct}
+    <ProductDetail product={cachedProduct} />
+  {:else}
+    {#await $fetchedProducts}
+      <ProductSkeleton />
+    {:then products}
+      <ProductDetail product={getProduct(products)} />
+    {:catch err}
+      <h1>There was an error {err}</h1>
+    {/await}
+  {/if}
 </div>
