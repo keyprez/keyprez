212e4dd04cf42701498de47bd0f45c201c1c7534
Michal Petrik <misopetrik@gmail.com>
2022-11-21 08:34:32 +0100

    🐛 Fix add to cart confirmation (#135)
    * 🚚 Remove trailing slash
    
    * 🐛 Fix confirmation for first addition

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const endpoint =
   process.env.NODE_ENV === 'production'
-    ? 'https://qmw0sv0yz7.execute-api.eu-west-1.amazonaws.com/'
+    ? 'https://qmw0sv0yz7.execute-api.eu-west-1.amazonaws.com'
     : 'http://localhost:3000';
 // The key needs to be replaced with a production one once we go live.
 export const stripePublishableKey =
