2887ae658f9c08c018e3d002c2169bfbd55211b2
vikingviolinist <misopetrik@gmail.com>
2022-11-22 12:30:17 +0100

    💄 Allow for extra classes

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,15 +1,16 @@
-<script>
+<script lang="ts">
   import { Loader } from '$lib';
 
-  export let text;
-  export let type;
+  export let text: string;
+  export let type: string;
   export let loading = false;
   export let onClick;
   export let disabled = false;
+  export let extraClassNames: string;
 </script>
 
 <button
-  class="relative flex justify-center w-full px-20 py-6 bg-black {disabled
+  class="relative flex justify-center w-full px-20 py-6 bg-black {extraClassNames} {disabled
     ? 'opacity-50'
     : 'hover:bg-teal-900 dark:hover:bg-teal-900'} rounded-lg md:max-w-xs dark:bg-teal-800"
   {type}
